4998fce083fa062295c441dcf24b39a8e1247606
djwirz <djwirz@gmail.com>
2025-03-20 15:33:38 -0500

    Failing test with fake response

--- petfinder/petfinder.test.ts
+++ petfinder/petfinder.test.ts
@@ -1,7 +1,11 @@
 import { describe, expect, test } from "vitest";
 import { getPets } from "./petfinder";
 
-test("Petfinder API should return an array of pets", async () => {
-  const response = await getPets();
-  expect(response.pets).toBeInstanceOf(Array);
+describe("Petfinder API", () => {
+  test("Should return an array of pets", async () => {
+    const response = await getPets();
+    expect(response).toHaveProperty("pets");
+    expect(response.pets).toBeInstanceOf(Array);
+    expect(response.pets.length).toBeGreaterThan(0);
+  });
 });
